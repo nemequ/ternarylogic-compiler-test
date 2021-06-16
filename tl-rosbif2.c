0c85fdc1f76627f3717ec031ed0db2027d030272
Christopher Moore <moore@free.fr>
2021-06-16 08:35:02 +0200

    SIMDE_VECTOR_SUBSCRIPT_OPS version
    Add SIMDE_VECTOR_SUBSCRIPT_OPS version avoiding some inefficient clang code.

--- tl-rosbif2.c
+++ tl-rosbif2.c
@@ -43,6 +43,148 @@ simde_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int 
 
   return r;
 }
+#elif defined(SIMDE_VECTOR_SUBSCRIPT_OPS)
+SIMDE_FUNCTION_ATTRIBUTES
+simde__m128i
+simde_mm_ternarylogic_epi32(simde__m128i a, simde__m128i b, simde__m128i c, int imm8)
+  SIMDE_REQUIRE_CONSTANT_RANGE(imm8, 0, 255) {
+  simde__m128i_private
+    r_, t_,
+    a_ = simde__m128i_to_private(a),
+    b_ = simde__m128i_to_private(b),
+    c_ = simde__m128i_to_private(c);
+  int to_do, mask;
+
+  to_do = imm8;
+
+  r_.u64 = a_.u64 ^ a_.u64;
+
+  mask = 0xFF;
+  if ((to_do & mask) == mask) {
+    r_.u64 = ~r_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0xF0;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 = a_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0xCC;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= b_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0xAA;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= c_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x0F;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= ~a_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x33;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= ~b_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x55;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= ~c_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x3C;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= a_.u64 ^ b_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x5A;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= a_.u64 ^ c_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x66;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= b_.u64 ^ c_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0xA0;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= a_.u64 & c_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x50;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= ~c_.u64 & a_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x0A;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= ~a_.u64 & c_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x88;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= b_.u64 & c_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x44;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= ~c_.u64 & b_.u64;
+    to_do &= ~mask;
+  }
+
+  mask = 0x22;
+  if ((to_do & mask) && ((imm8 & mask) == mask)) {
+    r_.u64 |= ~b_.u64 & c_.u64;
+    to_do &= ~mask;
+  }
+
+  if (to_do & 0xc0) {
+    t_.u64 = a_.u64 & b_.u64;
+    if ((to_do & 0xc0) == 0xc0) r_.u64 |= t_.u64;
+    else if (to_do & 0x80)      r_.u64 |=  c_.u64 & t_.u64;
+    else                        r_.u64 |= ~c_.u64 & t_.u64;
+  }
+
+  if (to_do & 0x30) {
+    t_.u64 = ~b_.u64 & a_.u64;
+    if ((to_do & 0x30) == 0x30) r_.u64 |= t_.u64;
+    else if (to_do & 0x20)      r_.u64 |=  c_.u64 & t_.u64;
+    else                        r_.u64 |= ~c_.u64 & t_.u64;
+  }
+
+  if (to_do & 0x0c) {
+    t_.u64 = ~a_.u64 & b_.u64;
+    if ((to_do & 0x0c) == 0x0c) r_.u64 |= t_.u64;
+    else if (to_do & 0x08)      r_.u64 |=  c_.u64 & t_.u64;
+    else                        r_.u64 |= ~c_.u64 & t_.u64;
+  }
+
+  if (to_do & 0x03) {
+    t_.u64 = ~(a_.u64 | b_.u64);
+    if ((to_do & 0x03) == 0x03) r_.u64 |= t_.u64;
+    else if (to_do & 0x02)      r_.u64 |=  c_.u64 & t_.u64;
+    else                        r_.u64 |= ~c_.u64 & t_.u64;
+  }
+
+  return simde__m128i_from_private(r_);
+}
 #else
 /* clang generates crazy inefficient code for imm8 = 24, 26, 28, 30, 36, 38, 44, 52, 54, 56, 78, 88, 100, 114 */
 /* (I only tested up to 127 as I was doing this by hand) */
